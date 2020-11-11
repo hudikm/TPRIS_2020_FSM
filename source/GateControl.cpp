95097e8615373a7281213ed582580b8f7fab1936
Martin Hudik <hudikm@gmail.com>
2020-11-11 07:45:13 +0100

    Init

---
+++ source/GateControl.cpp
@@ -0,0 +1,62 @@
+/*
+ * FSM.cpp
+ *
+ *  Created on: 10. 11. 2020
+ *      Author: martin
+ */
+
+#include <GateControl.h>
+
+//***************************************************************************
+const etl::array<GateControl::transition, 6> GateControl::transitionTable = {
+		GateControl::transition(
+				States::IDLE,   //Current state
+				Events::CMD_OPEN, // Event
+				States::OPEN,  // New state
+				&GateControl::OnTranToOpen,
+				&GateControl::Guard),
+		GateControl::transition(
+				States::IDLE,   //Current state
+				Events::CMD_CLOSE, // Event
+				States::CLOSE,  // New state
+				&GateControl::OnTranToClose,
+				&GateControl::Guard),
+		GateControl::transition(
+				States::CLOSE,   //Current state
+				Events::S1, // Event
+				States::IDLE,  // New state
+				&GateControl::OnTranToIdle,
+				&GateControl::Guard),
+		GateControl::transition(
+				States::OPEN,   //Current state
+				Events::S2, // Event
+				States::IDLE,  // New state
+				&GateControl::OnTranToIdle,
+				&GateControl::Guard),
+		GateControl::transition(
+				Events::INIT, // Event
+				States::CLOSE,  // New state
+				&GateControl::OnTranToClose,
+				&GateControl::Guard),
+		GateControl::transition(
+				Events::INIT, // Event
+				States::IDLE,  // New state
+				&GateControl::OnTranToIdle,
+				&GateControl::NotGuard)
+		 };
+//***************************************************************************
+
+const etl::array<GateControl::state, 3> GateControl::stateTable = {
+		GateControl::state(
+				States::IDLE,
+				&GateControl::OnEnterIdle,
+				&GateControl::OnExitIdle)
+	    ,GateControl::state(
+	    		States::OPEN,
+				&GateControl::OnEnterOpen,
+				&GateControl::OnExitOpen
+				)
+		,GateControl::state(States::CLOSE,
+				&GateControl::OnEnterClose,
+				&GateControl::OnExitClose)
+};
---
+++ source/MKL25Z128xxx4_etlcpp.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2016-2020 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file    MKL25Z128xxx4_etlcpp.cpp
+ * @brief   Application entry point.
+ */
+
+#include <board.h>
+#include <clock_config.h>
+#include <etl/state_chart.h>
+#include <fsl_debug_console.h>
+#include <GateControl.h>
+#include <peripherals.h>
+#include <pin_mux.h>
+#include "fsl_lpsci.h"
+
+volatile bool newChar_flag = false;
+volatile char newChar;
+
+extern "C" void UART0_IRQHandler(void) {
+
+	if ((kLPSCI_RxDataRegFullFlag) & LPSCI_GetStatusFlags(UART0)) {
+		newChar_flag = true;
+		newChar = LPSCI_ReadByte(UART0);
+	}
+}
+
+/* TODO: insert other definitions and declarations here. */
+
+int main(void) {
+
+	GateControl gateControl { };
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+#endif
+
+
+	PRINTF("Etlcpp FSM\n");
+	gateControl.start(false);
+	gateControl.guard = true;
+	gateControl.process_event(Events::INIT);
+
+	gateControl.process_event(Events::S1);
+
+	while (1) {
+		if (newChar_flag) {
+			newChar_flag = false;
+			switch (newChar) {
+			case 'c':
+				gateControl.process_event(Events::CMD_CLOSE);
+				break;
+			case 'o':
+				gateControl.process_event(Events::CMD_OPEN);
+				break;
+			case '1':
+				gateControl.process_event(Events::S1);
+				break;
+			case '2':
+				gateControl.process_event(Events::S2);
+				break;
+			}
+		}
+
+	}
+
+	return 0;
+}
